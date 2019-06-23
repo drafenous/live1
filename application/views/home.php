9adaa4c202107b03878f968ac306363b085542fd
Rodrigo R. Almeidam <rodrigo.roberto.almeida@gmail.com>
2019-06-23 19:43:25 -0300

    first commit

--- application/views/home.php
+++ application/views/home.php
@@ -6,24 +6,7 @@
         </div>
 
         <div class="col-md-12">
-            <table id="table" class="table display table-hover table-striped">
-                <thead>
-                    <tr>
-                        <th>Código</th>
-                        <th>Nome</th>
-                        <th>Telefone</th>
-                        <th>CPF</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>0</td>
-                        <td>Rodrigo</td>
-                        <td>0123456789</td>
-                        <td>000.000.000-00</td>
-                    </tr>
-                </tbody>
-            </table>
+            <!-- DataTables -->
         </div>
     </div>
 </div>
